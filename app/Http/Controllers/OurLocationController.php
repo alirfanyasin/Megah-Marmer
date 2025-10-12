f7da623d731c21305bf3dd3e05bbd13a53c78db7
alirfanyasin <risayunita21@gmail.com>
2025-10-13 01:25:09 +0700

    get location

--- app/Http/Controllers/OurLocationController.php
+++ app/Http/Controllers/OurLocationController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Location;
 use Illuminate\Http\Request;
 
 class OurLocationController extends Controller
 {
     public function index()
     {
-        return view('our-locations');
+        $locationData = Location::all();
+        return view('our-locations', compact('locationData'));
     }
 }
